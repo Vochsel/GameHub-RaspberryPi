be6345b5839ac3e77030261c506a58fdc7a524d7
Ben Skinner <benjamin.skinner96@gmail.com>
2017-11-11 00:58:58 +0000

    Fixed hostapd service not starting reliably

--- switch_host_mode.sh
+++ switch_host_mode.sh
@@ -20,7 +20,10 @@ sudo cp ./host_conf/hostapd.conf /etc/hostapd/hostapd.conf
 sudo cp ./host_conf/hostapd /etc/default/hostapd
 
 # stop hostapd
-sudo service hostapd start
+#sleep 3s
+#sudo service hostapd stop
+#sleep 3s
+#sudo service hostapd start
 
 # DNSMASQ
 
@@ -40,4 +43,11 @@ sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -
 
 sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 
+sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+
+sudo service hostapd stop
+sleep 3s
+sudo service hostapd start
